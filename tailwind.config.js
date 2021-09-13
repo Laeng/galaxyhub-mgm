0c9c4f5d489e4a083b7a94ac1d52960e74119455
Laeng <Laeng@users.noreply.github.com>
2021-09-13 11:29:54 +0900

    Add alpinejs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  purge: [],
-  darkMode: 'media', // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    mode: 'jit',
+    purge: [
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php'
+    ],
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Pretendard', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
 }
