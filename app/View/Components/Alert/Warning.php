bc46543b7c7cbdfeea0ff23decda45d7f4216cd5
Laeng <Laeng@users.noreply.github.com>
2021-09-18 15:33:32 +0900

    Add alert components

---
+++ app/View/Components/Alert/Warning.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\View\Components\Alert;
+
+use Closure;
+use Illuminate\View\Component;
+use Illuminate\View\View;
+
+class Warning extends Component
+{
+    public string $title;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($title = "")
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return View|Closure|string
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.alert.warning');
+    }
+}
