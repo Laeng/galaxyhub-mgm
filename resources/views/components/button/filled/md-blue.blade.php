e831ac0a63b32bca2e85e7a731005e36a8ae69e5
Laeng <Laeng@users.noreply.github.com>
2021-09-19 19:46:50 +0900

    Add privacy and rules contents

---
+++ resources/views/components/button/filled/md-blue.blade.php
@@ -0,0 +1,3 @@
+<button {{ $attributes->except('class') }} class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-transparent focus:ring-blue-500 {{ $class }}">
+    {{ $slot }}
+</button>
