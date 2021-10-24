1903feaa223ef04bae40ce9104ecd71787d62beb
Laeng <Laeng@users.noreply.github.com>
2021-10-25 00:36:26 +0900

    Add Steam data save process

--- app/Http/Controllers/Join/ViewJoinController.php
+++ app/Http/Controllers/Join/ViewJoinController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Join;
 
+use App\Action\Steam\Steam;
 use App\Action\Survey\SurveyForm;
 use App\Action\Group\Group;
+use App\Action\UserData\UserData;
 use App\Http\Controllers\Controller;
+use App\Jobs\ProcessSteamEnquiry;
 use App\Models\SurveyEntry;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
@@ -39,7 +42,7 @@ class ViewJoinController extends Controller
         return view('join.apply', ['survey' => $survey, 'action' => route('join.submit')]);
     }
 
-    public function submit(Request $request, Group $group, SurveyForm $form): RedirectResponse
+    public function submit(Request $request, Group $group, SurveyForm $form, Steam $steam, UserData $userData): RedirectResponse
     {
         if ($group->has([$group::ARMA_MEMBER, $group::BANNED, $group::ARMA_APPLY])) {
             return redirect()->route('lounge.index');
@@ -48,11 +51,21 @@ class ViewJoinController extends Controller
         $survey = $form->getJoinApplicationForm();
         $answers = $this->validate($request, $survey->validateRules());
 
+        $userId = $request->user()->id;
+        $profile = $steam->getPlayerSummaries($userId)[0];
+
+        if ($profile->communityVisibilityState != 3) {
+            return redirect()->back()->withErrors(['error' => '스팀 프로필이 친구 공개 또는 비공개 상태입니다. 프로필을 공개로 변경해 주십시오.']);
+        }
+
+        ProcessSteamEnquiry::dispatch($userId);
+
         (new SurveyEntry())->for($survey)->by(auth()->user())->fromArray($answers)->push();
 
-        auth()->user()->update(['agreed_at', now()]);
-        $group->create($group::ARMA_APPLY);
+        $now = now();
+        auth()->user()->update(['created_at', 'agreed_at'], [$now, $now]);
+        $group->add($group::ARMA_APPLY);
 
-        return redirect()->route('lounge.index');
+        return redirect()->route('lounge.index')->withErrors(['success' => '가입 신청이 접수되었습니다.']);
     }
 }
