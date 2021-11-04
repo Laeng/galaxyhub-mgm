855b7b65f02e08e2b6c2919b79f1de66f36b3cfc
Laeng <Laeng@users.noreply.github.com>
2021-11-04 18:10:34 +0900

    Fix font size

--- resources/views/components/survey/question/type/number.blade.php
+++ resources/views/components/survey/question/type/number.blade.php
@@ -1,5 +1,5 @@
 <label class="block">
-    <p class="text-lg leading-6 font-medium text-gray-900 mb-0.5">{!! $question->title !!} {!! in_array('required', $question->rules) ? '<span class="text-red-600 text-base">*</span>' : '' !!}</p>
+    <p class="leading-6 font-medium text-gray-900 mb-0.5">{!! $question->title !!} {!! in_array('required', $question->rules) ? '<span class="text-red-600 text-base">*</span>' : '' !!}</p>
     @if (is_null($answer))
         <p class="text-sm text-gray-500 mb-2">{!! $question->content !!}</p>
         <input type="number" name="{{ $question->key }}" id="{{ $question->key }}" value="{{ old($question->key) }}" {{ in_array('required', $question->rules) ? 'required' : '' }}
@@ -8,6 +8,6 @@
             <p class="text-sm text-red-700 mt-1">{{ $message }}</p>
         @enderror
     @else
-        <p>{{ $answer }}</p>
+        <p class="text-sm">{{ $answer }}</p>
     @endif
 </label>
--- resources/views/components/survey/question/type/radio.blade.php
+++ resources/views/components/survey/question/type/radio.blade.php
@@ -1,5 +1,5 @@
 <fieldset>
-    <legend class="text-lg leading-6 font-medium text-gray-900 mb-0.5">{!! $question->title !!} {!! in_array('required', $question->rules) ? '<span class="text-red-600 text-base">*</span>' : '' !!}</legend>
+    <legend class="leading-6 font-medium text-gray-900 mb-0.5">{!! $question->title !!} {!! in_array('required', $question->rules) ? '<span class="text-red-600 text-base">*</span>' : '' !!}</legend>
     @if (is_null($answer))
         <p class="text-sm text-gray-500 mb-2">{!! $question->content !!}</p>
         <div class="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-4">
@@ -15,6 +15,6 @@
         <p class="text-sm text-red-700 mt-1">{{ $message }}</p>
         @enderror
     @else
-        <p>{{ $answer }}</p>
+        <p class="text-sm">{{ $answer }}</p>
     @endif
 </fieldset>
