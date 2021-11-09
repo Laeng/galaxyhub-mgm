c21ca5457896ba9be262a0b4add0250a5189c50e
Laeng <Laeng@users.noreply.github.com>
2021-11-09 18:06:31 +0900

    Add application rejected and deferred blade

--- app/Http/Controllers/Lounge/ViewLoungeController.php
+++ app/Http/Controllers/Lounge/ViewLoungeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Lounge;
 
 use App\Action\Group\Group;
+use App\Action\PlayerHistory\PlayerHistory;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -12,28 +14,41 @@ use Illuminate\Http\Request;
 
 class ViewLoungeController extends Controller
 {
-    public function index(Request $request, Group $group): Factory|View|Application|RedirectResponse
+    public function index(Request $request, Group $group, PlayerHistory $history): Factory|View|Application|RedirectResponse
     {
-        try {
-            if ($group->has($group::BANNED)) {
-                return view('lounge.banned');
-            }
+        $user = $request->user();
+        $groups = $group->getUserGroups($user)->pluck(['group_id'])->toArray();
 
-            if ($group->has($group::INACTIVE)) {
-                return view('lounge.inactivated');
-            }
+        if (in_array( Group::BANNED, $groups)) return view('lounge.main.banned');
+        if (in_array( Group::INACTIVE, $groups)) return view('lounge.main.inactivated');
 
-            if ($group->has($group::ARMA_APPLY)) {
-                return view('lounge.applied');
-            }
+        // 거절
+        if (in_array( Group::ARMA_REJECT, $groups)) {
+            $rejectHistory = $this->getPlayerHistory($user, $history, PlayerHistory::TYPE_APPLICATION_REJECTED);
+            $x = count($rejectHistory);
+            $date = $rejectHistory[0]->created_at;
+            $reason = $rejectHistory[0]->description;
 
-            if (!$group->has($group::ARMA_MEMBER)) {
-                return redirect()->route('join.agree');
-            }
-        } catch (\Exception $e) {
-            abort(500, $e->getMessage());
+            return view('lounge.main.rejected', ['x' => $x, 'date' => $date, 'reason' => $reason]);
         }
 
-        return view('lounge.index');
+        // 보류
+        if (in_array( Group::ARMA_DEFER, $groups)) {
+            $rejectHistory = $this->getPlayerHistory($user, $history, PlayerHistory::TYPE_APPLICATION_REJECTED);
+            $reason = $rejectHistory[0]->description;
+
+            return view('lounge.main.deferred', ['reason' => $reason]);
+        }
+
+        if (in_array( Group::ARMA_APPLY, $groups)) return view('lounge.main.applied');
+        if (!in_array(Group::ARMA_MEMBER, $groups)) return view('join.agree');
+
+
+        return view('lounge.main.index');
+    }
+
+    private function getPlayerHistory(User $user, PlayerHistory $history, String $type) {
+        $social = $user->socials()->where('social_provider', 'steam')->latest()->first();
+        return $history->getModel($social->social_id, $type)->latest()->get();
     }
 }
---
+++ resources/views/lounge/main/deferred.blade.php
@@ -0,0 +1,18 @@
+<x-sub-page website-name="MGM Lounge" title="메인">
+    <x-section.basic parent-class="py-4 sm:py-6" class="flex justify-center items-center">
+        <div class="w-full md:w-2/3">
+            <div class="py-4 lg:p-16">
+                <h1 class="text-2xl lg:text-3xl font-bold text-center my-4 lg:mt-0 lg:mb-6">가입 보류 안내</h1>
+                <div class="text-center">
+                    <p>가입이 보류되었습니다. 아래 보류 사유를 확인 하신 후 다시 신청해주시기 바랍니다.</p>
+                    <p class="font-medium my-4">{{ $reason }}</p>
+                </div>
+                <div class="flex justify-center pt-6 pb-4 lg:pb-0 space-x-2">
+                    <x-button.filled.md-white type="button" onclick="location.href='{{ route('join.agree') }}'">
+                        다시 신청하기
+                    </x-button.filled.md-white>
+                </div>
+            </div>
+        </div>
+    </x-section.basic>
+</x-sub-page>
