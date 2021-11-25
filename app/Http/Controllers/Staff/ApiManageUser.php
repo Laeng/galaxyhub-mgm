56828a2de02657dc7f8608a40ecaa28ba7e226b2
Laeng <Laeng@users.noreply.github.com>
2021-11-25 15:36:56 +0900

    Add Player History viewer

---
+++ app/Http/Controllers/Staff/ApiManageUser.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Staff;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ApiManageUser extends Controller
+{
+
+}
