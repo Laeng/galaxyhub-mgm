e2b4ebf48afaae35a623b912e972ff83d3b340fd
Laeng <Laeng@users.noreply.github.com>
2021-12-13 16:49:29 +0900

    Fix components font size

--- resources/views/components/input/text/primary.blade.php
+++ resources/views/components/input/text/primary.blade.php
@@ -1,1 +1,1 @@
-<input {{ $attributes->except(['class', 'type']) }} class="shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border-gray-300 rounded-md {{ $class }}" type="{{ $type }}">
+<input {{ $attributes->except(['class', 'type']) }} class="shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full text-sm border-gray-300 rounded-md {{ $class }}" type="{{ $type }}">
