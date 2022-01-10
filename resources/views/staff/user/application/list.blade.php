250f5714beceea7ca2de1bed3f83a5006b8fc941
Laeng <Laeng@users.noreply.github.com>
2022-01-10 11:26:02 +0900

    Improve application manage alert

--- resources/views/staff/user/application/list.blade.php
+++ resources/views/staff/user/application/list.blade.php
@@ -7,12 +7,14 @@
             <div class="bg-white rounded-lg p-4 lg:p-16">
                 <h1 class="text-2xl lg:text-3xl font-bold text-center lg:text-left my-4 lg:mt-0 lg:mb-6">{{$title}}</h1>
 
-                <div class="flex-col space-y-2 mb-4">
-                    @foreach($alerts as $alert)
-                        <x-dynamic-component :component="'alert.' .$alert[0]" :title="$alert[1]">
-                            {!! $alert[2] !!}
-                        </x-dynamic-component>
-                    @endforeach
+                <div class="space-y-3 mb-3">
+                    <x-alert.info title="확인해주십시오!">
+                        <ul>
+                            <li>{{ now()->subYears(18)->year . '년생 이상만 가입을 허용해 주십시오. (' . now()->year . '년 기준)' }}</li>
+                            <li>미비 사항이 있다면 무조건 거절하시지 마시고 보류 처리 후 해당 부분을 보충할 기회를 주십시오.</li>
+                            <li>거절, 보류 사유는 해당 신청자에게 표시됩니다. 민감한 사항은 '유저 기록' 에 별도 기록해 주십시오.</li>
+                        </ul>
+                    </x-alert.info>
                 </div>
 
                 <div class="flex flex-col mb-4">
