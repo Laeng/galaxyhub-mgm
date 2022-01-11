d08c68164f962d7073845566aa895f3072a46df2
Laeng <Laeng@users.noreply.github.com>
2022-01-11 20:10:52 +0900

    Add mission survey contents

--- app/Action/Survey/SurveyForm.php
+++ app/Action/Survey/SurveyForm.php
@@ -117,9 +117,70 @@ class SurveyForm implements SurveyFormContract
         $one = $survey->sections()->create([
             'name' => '만족도 조사',
             'description' =>
-                "<p>본 미션은 아르마3를 함께 즐기고자 {$user->nickname}님께서 제작해 주셨으며 설문 결과는 {$user->nickname}님께 익명으로 제공됩니다.</p>".
-                '<p>설문과 관련된 분쟁 발생시 원활한 중재를 위해 MGM 아르마 클랜 스탭이 회원님의 설문 응답을 조회할 수 있는 점 양해 부탁드립니다.</p>'.
-                '<p>출석체크는 본 만족도 조사가 끝나면 하실 수 있으며 잠시 시간을 내어 참여해주시면 감사드립니다.</p>'
+                "<p>본 미션은 아르마3를 함께 즐기고자 {$user->nickname}님께서 제작해 주셨으며 설문 결과는 {$user->nickname}님께 익명으로 제공됩니다.".
+                '설문과 관련된 분쟁 발생시 원활한 중재를 위해 MGM 아르마 클랜 스탭이 회원님의 설문 응답을 조회할 수 있는 점 양해 부탁드립니다.</p>'.
+                '<p>출석체크는 본 만족도 조사가 끝나면 하실 수 있으며 잠시 시간을 내어 참여해주시면 감사드립니다.</p>'.
+                '<p class="text-red-600 text-base">* 필수 입력</p>'
+        ]);
+
+        $one->questions()->create([
+            'title' => '적군의 수는 적절 하였나요?',
+            'content' => '',
+            'type' => 'radio',
+            'options' => ['매우 만족', '만족', '보통', '불만족', '매우 불만족'],
+            'rules' => ['required']
+        ]);
+
+        $one->questions()->create([
+            'title' => '같이 플레이한 유저들과 협동이 잘 되었다고 생각하시나요? ',
+            'content' => '',
+            'type' => 'radio',
+            'options' => ['매우 만족', '만족', '보통', '불만족', '매우 불만족'],
+            'rules' => ['required']
+        ]);
+
+        $one->questions()->create([
+            'title' => '작전 진행이 원활하였다고 생각하시나요?',
+            'content' => '',
+            'type' => 'radio',
+            'options' => ['매우 만족', '만족', '보통', '불만족', '매우 불만족'],
+            'rules' => ['required']
+        ]);
+
+        $one->questions()->create([
+            'title' => '진행시간은 적절하였나요?',
+            'content' => '',
+            'type' => 'radio',
+            'options' => ['매우 만족', '만족', '보통', '불만족', '매우 불만족'],
+            'rules' => ['required']
+        ]);
+
+        $one->questions()->create([
+            'title' => '분대장과 분대원간 소통은 원할했나요?',
+            'content' => '',
+            'type' => 'radio',
+            'options' => ['매우 만족', '만족', '보통', '불만족', '매우 불만족'],
+            'rules' => ['required']
+        ]);
+
+        $one->questions()->create([
+            'title' => '착용한 장비와 미션의 난이도가 적절하였나요?',
+            'content' => '',
+            'type' => 'radio',
+            'options' => ['매우 만족', '만족', '보통', '불만족', '매우 불만족'],
+            'rules' => ['required']
+        ]);
+
+        $one->questions()->create([
+            'title' => '추가로 말씀하시고 싶은 것이 있나요?',
+            'content' => '회원님의 피드백은 더 나은 미션을 만드는데 큰 도움이 됩니다.',
+            'type' => 'long-text',
+            'rules' => []
+        ]);
+
+        $two = $survey->sections()->create([
+            'name' => '수고하셨습니다!',
+            'description' => '<p>모든 설문이 끝났습니다. 아래 버튼을 눌러 출석 체크를 진행 할 수 있습니다.</p>'
         ]);
 
         return $survey;
