f426ae5e2883ada436829f8c62933cb8cba4ba1e
Laeng <Laeng@users.noreply.github.com>
2022-02-15 12:01:47 +0900

    Add alert components

---
+++ app/View/Components/Alert/Galaxyhub/Info.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\View\Components\Alert\Galaxyhub;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Info extends Component
+{
+    public string $title;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(string $title)
+    {
+        $this->title = $title;
+    }
+
+    public function render(): View
+    {
+        return view('components.alert.galaxyhub.info');
+    }
+}
---
+++ resources/views/components/alert/galaxyhub/danger.blade.php
@@ -0,0 +1,15 @@
+<div class="rounded-md bg-red-50 p-4">
+    <div class="flex">
+        <div class="flex-shrink-0">
+            <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path fill="currentColor" d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 464c-114.7 0-208-93.31-208-208S141.3 48 256 48s208 93.31 208 208S370.7 464 256 464zM256 304c13.25 0 24-10.75 24-24v-128C280 138.8 269.3 128 256 128S232 138.8 232 152v128C232 293.3 242.8 304 256 304zM256 337.1c-17.36 0-31.44 14.08-31.44 31.44C224.6 385.9 238.6 400 256 400s31.44-14.08 31.44-31.44C287.4 351.2 273.4 337.1 256 337.1z"></path>
+            </svg>
+        </div>
+        <div class="ml-3">
+            <h3 class="text-sm font-bold text-red-800">{{ $title }}</h3>
+            <div class="mt-2 text-sm text-red-700">
+                {!! $slot !!}
+            </div>
+        </div>
+    </div>
+</div>
